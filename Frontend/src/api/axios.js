bceba9d75bec35f62b251d14825c1a58c8f71472
amanshiv123 <akhandsingh8303844970@gmail.com>
2026-06-30 22:47:17 +0530

    change Axios url

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-   baseURL: "https://my-backend.vercel.app/api",
+   baseURL: "https://my-backend.vercel.app",
   withCredentials: true,
 });
 
